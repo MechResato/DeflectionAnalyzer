b82264ca7c0f21bc39790e0443b109a75f5158ed
Rene San <rene.san@gmx.at>
2021-03-29 10:51:43 +0200

    Test record float

--- record.c
+++ record.c
@@ -21,9 +21,13 @@ void record_buffer(void)
 {
 	// Example from Infineon for test purpose. TODO
 
+	float testf_w = 1.0/7.0;
+	float testf_r = 0.0;
+
+
 	FRESULT res; /* API result code */
 	UINT bw,br; /* Bytes written */
-	char buff[25];
+	char buff[50];
 
 	/* Register work area */
 	res = f_mount(&fs, "0:", 1);
@@ -38,27 +42,45 @@ void record_buffer(void)
 			if ((res == FR_OK) || (res == FR_EXIST))
 			{
 				/* Write a message */
-				sprintf(buff,"Current value %d\n", InputBuffer1[InputBuffer1_idx]);
-				res = f_write(&fil, buff, 15, &bw);
+				sprintf(buff,"%a\n", testf_w);
+				printf("init: %s\n", buff);
+				res = f_write(&fil, buff, 13, &bw);
+				//sprintf(buff,"Current value 2 %5d\n", InputBuffer1[InputBuffer1_idx]);
+				//res = f_write(&fil, buff, 22, &bw);
 				if (res == FR_OK )
 				{
 					res = f_lseek(&fil, 0);
-					/* Read the buffer from file */
-					res = f_read(&fil, &buff[0], 15, &br);
-					if(res == FR_OK)
-					{
-						/* Go to location 15 */
-						res = f_lseek(&fil, 15);
-						if(res == FR_OK)
-						{
-							/* Add some more content to the file */
-							res = f_write(&fil, "\nWelcome to Infineon", 20, &bw);
-							if(res == FR_OK)
-							{
 
-							}
-						}
-					}
+					//sprintf(Buffer2,"%08lX",*(unsigned long*)&ADCValue2_float2);
+					printf("ori: %08lX, %f\n", *(unsigned long*)&testf_w, testf_w);
+					f_gets(buff, 25, &fil);
+					printf("gets: %s\n", buff);
+
+					testf_r = strtof(buff, NULL);
+					printf("strtof: %a, %f\n", testf_r, testf_r);
+
+					//f_gets(buff, 25, &fil);
+					//printf("2 %s\n", buff);
+
+					/* Read the buffer from file */
+					//res = f_read(&fil, &buff[0], 15, &br);
+					//if(res == FR_OK)
+					//{
+					//	/* Go to location 15 */
+					//	res = f_lseek(&fil, 15);
+					//	if(res == FR_OK)
+					//	{
+					//		/* Add some more content to the file */
+					//		res = f_write(&fil, "\nWelcome to Infineon", 20, &bw);
+					//		if(res == FR_OK)
+					//		{
+					//
+					//		}
+					//	}
+					//}
+				}
+				else{
+					printf("Write Failed\n");
 				}
 			}
 			f_close(&fil);
@@ -68,3 +90,47 @@ void record_buffer(void)
 
 
 }
+
+
+
+
+/* Register work area */
+//res = f_mount(&fs, "0:", 1);
+//if (res == FR_OK)
+//{
+//	/* Create a new directory */
+//	res = f_mkdir("Record");
+//	if ((res == FR_OK) || (res == FR_EXIST))
+//	{
+//		/* Create a file as new */
+//		res = f_open(&fil, "Record/log.txt", FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
+//		if ((res == FR_OK) || (res == FR_EXIST))
+//		{
+//			/* Write a message */
+//			sprintf(buff,"Current value %d\n", InputBuffer1[InputBuffer1_idx]);
+//			res = f_write(&fil, buff, 15, &bw);
+//			if (res == FR_OK )
+//			{
+//				res = f_lseek(&fil, 0);
+//				/* Read the buffer from file */
+//				res = f_read(&fil, &buff[0], 15, &br);
+//				if(res == FR_OK)
+//				{
+//					/* Go to location 15 */
+//					res = f_lseek(&fil, 15);
+//					if(res == FR_OK)
+//					{
+//						/* Add some more content to the file */
+//						res = f_write(&fil, "\nWelcome to Infineon", 20, &bw);
+//						if(res == FR_OK)
+//						{
+//
+//						}
+//					}
+//				}
+//			}
+//		}
+//		f_close(&fil);
+//	}
+//	res = f_unmount("0:");
+//}
